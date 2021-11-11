89728b61d84640aab55814b6c4f68f3ad9e01810
DaehyeonKim <Daehyeonoff@gmail.com>
2021-11-11 16:19:49 +0900

    20211111

---
+++ App.js
@@ -0,0 +1,169 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import type {Node} from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  PlatformColor,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Task from './component/Task';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAssTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null); 
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View style={styles.container}>
+    <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}>Today's tasks</Text>
+   <View style={styles.items}>
+           {
+             taskItems.map((item, index) => {
+               return (
+                <TouchableOpacity onPress={() => completeTask(index)}>
+                <Task key={index} text={item} />     
+                </TouchableOpacity>
+
+               ) 
+              })
+           }
+         
+          
+   </View>
+    
+    </View>
+    <KeyboardAvoidingView behavior={Platform.OS === "ms" ? "padding" : "height"} 
+    style = {styles.writeTaskWrapper}>
+     <TextInput style={styles.input} placeholder={'Write a Task'} value={task} onChangeText={text => setTask(text)}/>
+     <TouchableOpacity  onPress={ () => handleAssTask()}>
+       <View style={styles.addWrapper}>
+         <Text style={styles.addText}>+</Text>
+       </View>
+     </TouchableOpacity>
+ 
+    </KeyboardAvoidingView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#E8EAED' ,
+  },
+ tasksWrapper: {
+   paddingTop: 80,
+   paddingHorizontal: 20,
+ },
+ sectionTitle:{
+   fontSize: 24,
+   fontWeight:'bold'
+ } ,
+ items:{
+  marginTop:30,
+ },
+ writeTaskWrapper:{
+   position:'absolute',
+   bottom: 60,
+   width: '100%',
+   flexDirection:"row",
+   justifyContent: "space-around",
+   alignItems: 'center'
+ },
+ input:{
+   paddingVertical: 15,
+   width: 250,
+   paddingHorizontal: 15,
+   backgroundColor: "#FFF",
+   borderRadius: 60,
+   borderColor: "#C0C0C0",
+   borderWidth: 1,
+   
+ },
+ addWrapper:{
+  width: 60,
+  height: 60,
+  backgroundColor: "#FFF",
+  borderRadius:60,
+  justifyContent: "center",
+  alignItems: 'center',
+  borderColor: "#C0C0C0",
+  borderWidth: 1,
+
+
+ },
+ addText:{},
+ 
+});
+
+export default App;
